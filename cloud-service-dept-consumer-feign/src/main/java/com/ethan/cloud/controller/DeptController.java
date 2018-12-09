822612f08b65d5affceaf08b463ba65e1a6a6109
ethan <ethan89798@gmail.com>
2018-12-09 23:24:45 +0800

    增加feign

--- cloud-service-dept-consumer-feign/src/main/java/com/ethan/cloud/controller/DeptController.java
+++ cloud-service-dept-consumer-feign/src/main/java/com/ethan/cloud/controller/DeptController.java
@@ -3,10 +3,7 @@ package com.ethan.cloud.controller;
 import com.ethan.cloud.entities.Dept;
 import com.ethan.cloud.feign.service.DeptClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,6 +11,7 @@ import java.util.List;
  *
  */
 @RestController
+@RequestMapping(value = "/dept")
 public class DeptController {
 
     @Autowired
